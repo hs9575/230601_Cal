e2300c9450b687d026085bb7dd184129d1661ec0
hyunsung.jin <darkfishno1@gmail.com>
2023-06-01 10:06:50 +0900

    getMinus unittest도 업데이트합니다.

--- Cal-Test/test.cpp
+++ Cal-Test/test.cpp
@@ -5,3 +5,8 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseNameMinus, TestName) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(2, cal->getMinus(5,3));
+}
