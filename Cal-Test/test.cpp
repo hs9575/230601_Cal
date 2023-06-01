d81acc0b94a1c812b0c32fccde79c23ef290c203
hwangyu.kim <skykhk01@gmail.com>
2023-06-01 10:00:06 +0900

    Add getGop function and unittest

--- Cal-Test/test.cpp
+++ Cal-Test/test.cpp
@@ -5,3 +5,9 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, Gop_2_3)
+{
+	Cal* cal = new Cal();	
+	EXPECT_EQ(6, cal->getGop(2, 3));
+}
